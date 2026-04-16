3fc806851cbbba22f41a4798c364c36b2b8d0ad6
rkbyker <rkbyker@MacBook-Air-rkbyker.local>
2026-04-16 11:24:29 +0300

    fix: onBackPressed, remove duplicate bottom-nav CSS from profile

--- android/app/src/main/java/com/sharecar/app/MainActivity.java
+++ android/app/src/main/java/com/sharecar/app/MainActivity.java
@@ -1,5 +1,16 @@
 package com.sharecar.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onBackPressed() {
+        if (this.bridge != null && this.bridge.getWebView().canGoBack()) {
+            this.bridge.getWebView().goBack();
+        } else {
+            // минимизируем вместо закрытия
+            moveTaskToBack(true);
+        }
+    }
+}
